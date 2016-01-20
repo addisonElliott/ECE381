ac63ad6b34a06d9002feb6439d825c89e6373385
Addison Elliott <addison.elliott@gmail.com>
2016-01-19 23:18:27 -0600

    Added comments. Lab 1 done.

--- LCDModule/LCDModule/main.c
+++ LCDModule/LCDModule/main.c
@@ -1,50 +1,43 @@
-//----------------------------------------------------------------------------
-// C main line
-//----------------------------------------------------------------------------
-
 #include <m8c.h>        // part specific constants and macros
 #include "PSoCAPI.h"    // PSoC API definitions for all User Modules
 
 
 void main(void)
-{
-	// M8C_EnableGInt ; // Uncomment this line to enable Global Interrupts
-	// Insert your main routine code here.
+{	
+	unsigned char sw1On = FALSE; // Previous value of SW1
+	unsigned char sw2On = FALSE; // Previous value of SW2
+	unsigned int count = 0; // Variable that stores number
+	char tmpStr[4]; // Temporary string to store the decimal text
 	
-	unsigned char sw1On = FALSE;
-	unsigned char sw2On = FALSE; // Comment
-	unsigned int count = 0;
-	char tmpStr[4];
-	
-	LCD_Start();
+	LCD_Start(); // Start the LED
 	LCD_Position(1, 0);
-	LCD_PrCString("Count: 0");
+	LCD_PrCString("Count: 0"); // Display string
 	
 	while (1)
 	{
-		if (SW1_Data_ADDR & SW1_MASK)
+		if (SW1_Data_ADDR & SW1_MASK) // If SW1 is on
 		{
-			if (sw1On == FALSE)
+			if (sw1On == FALSE) // If previous value of SW1 was not on,
 			{
-				sw1On = TRUE;
-				if (count != 100)
+				sw1On = TRUE; // Set previous value to true
+				if (count != 100) // If not out of range
 				{
 					LCD_Position(0, 0);
 					LCD_PrCString("Incrementing");
 					count = count + 1;
 					LCD_Position(1, 7);
-					LCD_PrHexInt(count);
+					LCD_PrHexInt(count); // Display hex count
 					LCD_Position(1, 12);
-					LCD_PrCString("   ");
+					LCD_PrCString("   "); // Clear the previous decimal string in case the number of digits is different
 					LCD_Position(1, 12);
-					utoa(tmpStr, count, 10);
-					LCD_PrString(tmpStr);
+					utoa(tmpStr, count, 10); // Prints count as a string into tmpStr
+					LCD_PrString(tmpStr); // Display the decimal count
 				}
 			}
 		}
 		else
 		{
-			if (sw2On == FALSE)
+			if (sw2On == FALSE) // If both switches are not on, then clear the string in top row
 			{
 				LCD_Position(0, 0);
 				LCD_PrCString("            ");
@@ -52,29 +45,29 @@ void main(void)
 			sw1On = FALSE;
 		}
 		
-		if (SW2_Data_ADDR & SW2_MASK)
+		if (SW2_Data_ADDR & SW2_MASK) // If SW2 is on
 		{
-			if (sw2On == FALSE)
+			if (sw2On == FALSE) // If previous value of SW2 was not on,
 			{
-				sw2On = TRUE;	
-				if (count != 0)
+				sw2On = TRUE;	// Set previous value to true
+				if (count != 0) // If not out of range
 				{
 					LCD_Position(0, 0);
 					LCD_PrCString("Decrementing");
 					count = count - 1;
 					LCD_Position(1, 7);
-					LCD_PrHexInt(count);
+					LCD_PrHexInt(count); // Display hex count
 					LCD_Position(1, 12);
-					LCD_PrCString("   ");
+					LCD_PrCString("   "); // Clear the previous decimal string in case the number of digits is different
 					LCD_Position(1, 12);
-					utoa(tmpStr, count, 10);
-					LCD_PrString(tmpStr);
+					utoa(tmpStr, count, 10); // Prints count as a string into tmpStr
+					LCD_PrString(tmpStr); // Display the decimal count
 				}
 			}
 		}
 		else
 		{
-			if (sw1On == FALSE)
+			if (sw1On == FALSE) // If both switches are not on, then clear the string in top row
 			{
 				LCD_Position(0, 0);
 				LCD_PrCString("            ");
--- PollSwitch/PollSwitch/main.c
+++ PollSwitch/PollSwitch/main.c
@@ -1,40 +1,34 @@
-//----------------------------------------------------------------------------
-// C main line
-//----------------------------------------------------------------------------
-
 #include <m8c.h>        // part specific constants and macros
 #include "PSoCAPI.h"    // PSoC API definitions for all User Modules
 
 
 void main(void)
 {
-	// M8C_EnableGInt ; // Uncomment this line to enable Global Interrupts
-	// Insert your main routine code here.
-	unsigned char sw1On = FALSE;
-	unsigned char sw2On = FALSE;
+	unsigned char sw1On = FALSE; // Previous value of SW1
+	unsigned char sw2On = FALSE; // Previous value of SW2
 	
 	while (1)
 	{
-		if (SW1_Data_ADDR & SW1_MASK)
+		if (SW1_Data_ADDR & SW1_MASK) // If the switch is on
 		{
-			if (sw1On == FALSE)
+			if (sw1On == FALSE) // If the switch wasn't on previously
 			{
-				sw1On = TRUE;
-				LED1_Data_ADDR ^= LED1_MASK;
+				sw1On = TRUE; // Set previous value to on
+				LED1_Data_ADDR ^= LED1_MASK; // Toggle LED
 			}
 		}
 		else
-			sw1On = FALSE;
+			sw1On = FALSE; // Switch is not on, set the previous value
 		
 		if (SW2_Data_ADDR & SW2_MASK)
 		{
-			if (sw2On == FALSE)
+			if (sw2On == FALSE) // If the switch wasn't on previously
 			{
-				sw2On = TRUE;
-				LED2_Data_ADDR ^= LED2_MASK;
+				sw2On = TRUE; // Set the previous value to on
+				LED2_Data_ADDR ^= LED2_MASK; // Toggle LED
 			}
 		}
 		else
-			sw2On = FALSE;
+			sw2On = FALSE; // Switch is not on, set the previous value
 	}
 }
